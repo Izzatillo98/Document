073c7627198de1be9c707e3c9add9f8f014cb000
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-08-08 11:05:42 +0500

    Add project files.

---
+++ Document/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Document
+{
+    internal class Document
+    {
+        private string data;
+        private string filename;
+
+        public string opem
+    }
+}
---
+++ Document/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Document
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
